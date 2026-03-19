804d66d550eae660475dfb8cbdd0af6e1f4cbe58
AttlLukas <lukasa.2402@gmail.com>
2026-03-19 20:39:09 +0100

    lint fixes

--- frontend/app/src/lib/components/button.svelte
+++ frontend/app/src/lib/components/button.svelte
@@ -1,26 +1,32 @@
 <script lang="ts">
-  import { goto } from '$app/navigation';
-  const { label, href } = $props<{
-    label: string;
-    href: string;
-  }>();
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
+	const { label, href } = $props<{
+		label: string;
+		href: string;
+	}>();
 </script>
 
-<button onclick={() => goto(href)}>
-  {label}
+<button
+	onclick={async () => {
+		await goto(resolve(href));
+		resolve(href);
+	}}
+>
+	{label}
 </button>
 
 <style>
-  button {
-    padding: 0.75rem 1.5rem;
-    background: black;
-    color: white;
-    border: none;
-    border-radius: 6px;
-    cursor: pointer;
-  }
+	button {
+		padding: 0.75rem 1.5rem;
+		background: black;
+		color: white;
+		border: none;
+		border-radius: 6px;
+		cursor: pointer;
+	}
 
-  button:hover {
-    opacity: 0.9;
-  }
+	button:hover {
+		opacity: 0.9;
+	}
 </style>
