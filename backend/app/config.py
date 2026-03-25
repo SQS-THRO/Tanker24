7efe6e172ad26ced094084d74745bcc25a6a2605
AttlLukas <lukasa.2402@gmail.com>
2026-03-25 22:33:35 +0100

    change default url in docker, allow localhost and 127.0.0.1 on CORS

--- backend/app/config.py
+++ backend/app/config.py
@@ -14,7 +14,9 @@ class Settings(BaseSettings):
 	CORSorigins: list[str] = [
 		"http://localhost:5173",
 		"http://localhost:3000",
-	]
+		"http://127.0.0.1:5173",
+		"http://127.0.0.1:3000"
+		]
 
 	class Config:
 		env_file = ".env"
