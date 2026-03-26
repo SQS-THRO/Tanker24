ede21179a13f5300db28f72e1e02a96c2cf98fd5
VenetianMasquerade <207693667+VenetianMasquerade@users.noreply.github.com>
2026-03-26 14:40:00 +0100

    Changed regex because SonarQube does not like backtracking

--- frontend/app/src/routes/map/page.svelte.e2e.ts
+++ frontend/app/src/routes/map/page.svelte.e2e.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('redirects to login when not authenticated', async ({ page }) => {
 	await page.goto('/map');
-	await expect(page).toHaveURL(/.*\/login/);
+	await expect(page).toHaveURL(/\/login$/);
 });
