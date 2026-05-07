b788c8dcb415d478af85a1401cdca0bbe4af2ac0
AttlLukas <lukasa.2402@gmail.com>
2026-05-07 13:25:20 +0200

    make cors read the .env file

--- backend/app/main.py
+++ backend/app/main.py
@@ -58,7 +58,7 @@ app.include_router(api_router)
 
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=settings.CORSorigins,
+	allow_origins=settings.cors_origins,
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
