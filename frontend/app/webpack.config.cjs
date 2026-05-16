722f210e6b353488db717749f25a4a252aee8348
AttlLukas <lukasa.2402@gmail.com>
2026-05-16 18:46:51 +0200

    add rules and formatting

--- frontend/app/webpack.config.cjs
+++ frontend/app/webpack.config.cjs
@@ -1,9 +1,11 @@
+//this require needs to be ignored by eslint because webpack needs it to be commonjs
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const path = require('path');
 
 module.exports = {
-    resolve: {
-        alias: {
-            $lib: path.resolve(__dirname, 'src/lib')
-        }
-    }
+	resolve: {
+		alias: {
+			$lib: path.resolve(__dirname, 'src/lib')
+		}
+	}
 };
