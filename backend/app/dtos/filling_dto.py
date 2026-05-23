993498ad4199949ccfbc1037a4ef35643a572a34
Lukas Kielmann <lukas.kielmann@gmx.de>
2026-05-23 10:17:53 +0200

    #215 fix dto inheritence

--- backend/app/dtos/filling_dto.py
+++ backend/app/dtos/filling_dto.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+from pydantic import BaseModel
+
 from build.lib.app.models import FuelType
 
 
 @dataclass
-class FillingDTO:
+class FillingDTO(BaseModel):
 	license_plate_number: str
 	car_type: str
 	mileage: int
