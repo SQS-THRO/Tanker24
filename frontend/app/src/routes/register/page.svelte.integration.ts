3e0de8f039a516900e278128920eccde4dad357b
AttlLukas <lukasa.2402@gmail.com>
2026-06-11 16:58:31 +0200

    sonar seems to do pattern matching for passwords

--- frontend/app/src/routes/register/page.svelte.integration.ts
+++ frontend/app/src/routes/register/page.svelte.integration.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('can register and will be logged in after', async ({ page }) => {
 	const email = `test-${Date.now()}@example.com`;
-	const password = 'Test1234!';
+	const password = 'securePassword123!';
 
 	await page.goto('/register');
 
