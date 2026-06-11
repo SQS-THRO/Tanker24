69393a0bf8d683ebbf1546ddc96dfa000848ea61
AttlLukas <lukasa.2402@gmail.com>
2026-06-11 16:41:58 +0200

    init integration pipeline actions with playwright

---
+++ frontend/app/src/routes/register/page.svelte.integration.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@playwright/test';
+
+test('can register and will be logged in after', async ({ page }) => {
+	const email = `test-${Date.now()}@example.com`;
+	const password = 'Test1234!';
+
+	await page.goto('/register');
+
+	await page.locator('.modal-overlay .btn-secondary').click();
+
+	await page.fill('#first_name', 'Test');
+	await page.fill('#last_name', 'User');
+	await page.fill('#email', email);
+	await page.fill('#password', password);
+	await page.fill('#confirmPassword', password);
+	await page.fill('#invitationKey', '901563b82fa7adcbbc2a7e885f143c57');
+
+	await page.click('button[type="submit"]');
+
+	await page.waitForURL('**/account');
+
+	await expect(page.locator('.profile-header')).toBeVisible();
+});
